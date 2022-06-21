fe57e62becea89a95161b805d5356eccba956f1a
Alberto Martin Vidal <81685321+mvalberto@users.noreply.github.com>
2022-06-21 17:18:46 +0200

    Add files via upload

---
+++ Sin_installer/check_mem.ps1
@@ -0,0 +1,32 @@
+﻿# Definir codigos de salida
+
+$STATE_OK = 0
+$STATE_WARNING = 1
+$STATE_CRITICAL = 2
+$STATE_UNKNOWN = 3
+
+$SysMem = Get-CIMInstance Win32_OperatingSystem
+$pctFree = [math]::Round(($SysMem.FreePhysicalMemory/$SysMem.TotalVisibleMemorySize)*100,2)
+
+if ($pctFree -ge 45) {
+$SysMem = Get-CIMInstance Win32_OperatingSystem
+"Memoria libre {0:N2} GB de un total de {1:N2} GB " -f   
+($SysMem.FreePhysicalMemory/1MB),    
+($SysMem.TotalVisibleMemorySize/1MB) | Write-Host
+exit $STATE_OK
+}
+elseif ($pctFree -ge 15 ) {
+$SysMem = Get-CIMInstance Win32_OperatingSystem
+"Memoria libre {0:N2} GB de un total de {1:N2} GB " -f   
+($SysMem.FreePhysicalMemory/1MB),    
+($SysMem.TotalVisibleMemorySize/1MB) | Write-Host
+exit $STATE_WARNING
+}
+else {
+$SysMem = Get-CIMInstance Win32_OperatingSystem
+"Memoria libre {0:N2} GB de un total de {1:N2} GB, Optimizando memoria con RAMRush..." -f   
+($SysMem.FreePhysicalMemory/1MB),    
+($SysMem.TotalVisibleMemorySize/1MB) | Write-Host
+start-process "C:\Program Files\NSClient++\scripts\ramrush.bat"
+exit $STATE_CRITICAL
+}
---
+++ Sin_installer/ramrush.bat
@@ -0,0 +1,6 @@
+@echo off
+taskkill -IM RamRush.exe /F
+cd C:\Program Files\NSClient++\scripts\
+start RAMRush.exe "-AutoOptimize"
+ping -n 30 127.0.0.1 > nul
+taskkill -IM RamRush.exe /F
