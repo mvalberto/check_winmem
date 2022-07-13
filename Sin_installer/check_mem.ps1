d81ca425a0965df8e0962d938807081990ef1f5b
Alberto Martin Vidal <81685321+mvalberto@users.noreply.github.com>
2022-07-13 12:27:46 +0200

    Update check_mem.ps1

--- Sin_installer/check_mem.ps1
+++ Sin_installer/check_mem.ps1
@@ -1,21 +1,29 @@
-﻿# Definir codigos de salida
+# Pasar argumentos
+Param(
+    [Parameter(Mandatory=$false)][string]$ok,
+    [Parameter(Mandatory=$false)][string]$war
+    )
+
+# Definir codigos de salida
 
 $STATE_OK = 0
 $STATE_WARNING = 1
 $STATE_CRITICAL = 2
 $STATE_UNKNOWN = 3
 
+
+
 $SysMem = Get-CIMInstance Win32_OperatingSystem
 $pctFree = [math]::Round(($SysMem.FreePhysicalMemory/$SysMem.TotalVisibleMemorySize)*100,2)
 
-if ($pctFree -ge 45) {
+if ($pctFree -ge $ok) {
 $SysMem = Get-CIMInstance Win32_OperatingSystem
 "Memoria libre {0:N2} GB de un total de {1:N2} GB " -f   
 ($SysMem.FreePhysicalMemory/1MB),    
 ($SysMem.TotalVisibleMemorySize/1MB) | Write-Host
 exit $STATE_OK
 }
-elseif ($pctFree -ge 15 ) {
+elseif ($pctFree -ge $war ) {
 $SysMem = Get-CIMInstance Win32_OperatingSystem
 "Memoria libre {0:N2} GB de un total de {1:N2} GB " -f   
 ($SysMem.FreePhysicalMemory/1MB),    
